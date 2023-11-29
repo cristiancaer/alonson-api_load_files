e556db0f2eddbef4f6cbc11ec5f17668f8ffa1a8
cristian espinosa <caespinosa@cenicana.org>
2023-11-28 23:43:38 -0500

    feat: add enpoint me

--- users/urls.py
+++ users/urls.py
@@ -3,12 +3,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.urls import path
-from .views import UsersApiView
+from .views import UsersApiView, MeApiView
 
 
 urlpatterns = [
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('', UsersApiView.as_view(), name='users'),
+    path('me', MeApiView.as_view(), name='users.me'),
     path('<int:user_id>', UsersApiView.as_view(), name='user'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from utils.views import RollAccessApiView
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UsersApiView(RollAccessApiView):
@@ -9,3 +12,11 @@ class UsersApiView(RollAccessApiView):
     model = User
     serializer = UserSerializer
     id_field_name = 'user_id'
+
+
+class MeApiView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
